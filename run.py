00c044adbc31939045053e8622ab2128233664d0
DrogrinHunter <lukewillmore67@gmail.com>
2021-08-26 21:20:24 +0000

    add sales data to sales worksheet

--- run.py
+++ run.py
@@ -17,21 +17,23 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10, 20, 30, 40, 50, 60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10, 20, 30, 40, 50, 60\n")
 
-    data_str = input("Enter your data here: ")
-    
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
-
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+    return sales_data
 
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into int, 
+    Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 vales.
     """
 
@@ -41,8 +43,22 @@ def validate_data(values):
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
-    except ValueError as e: 
+
+    except ValueError as e:
         print(f"Invalid data: {e}, please try again. \n")
+        return False
 
-get_sales_data()
+    return True
 
+def update_sales_worksheet(data):
+    """
+    updates sales worksheet
+    """
+    print("updating sales worksheet ... \n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("sales worksheet updated successfully.\n")
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
